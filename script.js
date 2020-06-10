3542f549d7aa600a353391b357408472fb173002
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-06-10 14:07:45 -0700

    added randomiser functions on letters, numbers and symbols

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var userInput = []; 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
 }
+
+randomCharacter = () => specialCharacters[Math.floor(Math.random()*specialCharacters.length)]
+
+randomNumber = () => Math.floor(Math.random()*10);
+
+randomLetter = () => letters[Math.floor(Math.random()*letters.length)]
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
