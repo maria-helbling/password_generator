d506679aed3b9c187f4befce8bb4b95618a6cd28
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-06-10 18:29:18 -0700

    User inputs validated

--- script.js
+++ script.js
@@ -15,36 +15,47 @@ function writePassword() {
 
 }
 
-// PW generator function
-generatePassword = () => {
-  // input on PW length btw 8 and 128 inclusive
-  pwLength = prompt(`Hello! Welcome to your PW generator. \n\n Tell me how long do you need your PW to be. \n\n NB! Only numbers between 8 and 128 inclusive`)
-  // input is validated
-  if (parseInt(pwLength)>7 && parseInt(pwLength)<129) {
-        pwLength = parseInt(pwLength);
-        for (let i = 0; i < prompts.length-1; i++) {
-          let answer = confirm(prompts[i]);
-          userInput.push(answer);
-          console.log(answer, userInput);
-        }      
-    // we need user input on if they need lowercase, uppercase, numeric, and/or special characters
-      // input should be validated and at least one character type should be selected
-
-  } else {
-    
-    alert(`That was not a valid input, please start over!`)
-    console.log(pwLength);
-}
-}
-
 // functions to generate random symbol, number and letter
 randomCharacter = () => specialCharacters[Math.floor(Math.random()*specialCharacters.length)]
 
 randomNumber = () => Math.floor(Math.random()*10);
 
-randomLetter = () => letters[Math.floor(Math.random()*letters.length)]
+randomLetterL = () => letters[Math.floor(Math.random()*letters.length)]
 
+randomLetterU = () => randomLetterL().toUpperCase()
+var allFunct = [randomLetterL, randomLetterU, randomNumber, randomCharacter];
+var functArray = [];
 
+// PW generator function
+generatePassword = () => {
+  // user input on PW length btw 8 and 128 inclusive
+  pwLength = prompt(`Hello! Welcome to your PW generator. \n\n Tell me how long do you need your PW to be. \n\n NB! Only numbers between 8 and 128 inclusive`)
+  // input is validated
+  if (parseInt(pwLength)>7 && parseInt(pwLength)<129) {
+        pwLength = parseInt(pwLength);
+  } else {
+        alert(`That was not a valid input, please start over!`);
+        return "";
+  }
+  
+  // get user input on if they need lowercase, uppercase, numeric, and/or special characters
+  // identify functions needed to meet the spec and add them to array
+  let validator = 0 
+  for (i=0; i<prompts.length; i++) {
+    if (confirm(prompts[i])) {
+        functArray.push(allFunct[i]);
+        validator++
+      }
+  }
+                            
+  // input is validated, at least one character type should be selected, otherwise alert and exit function
+    if (validator===0) {
+    alert(`You need at least one type of character in your password for it to work.`)
+    return ""        
+  }
 
+  //generate password
+  
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
